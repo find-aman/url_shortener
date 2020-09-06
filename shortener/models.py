f2a003a877d4fac32fc2e9f48196d31426edd4b2
Aman Gupta <amang9578@gmail.com>
2020-09-07 01:09:24 +0530

    min max short code variables added in settings

--- shortener/models.py
+++ shortener/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from .utils import code_generator, create_shortcode
+from django.conf import settings
+
+
+SHORTCODE_MAX = getattr(settings, "SHORTCODE_MAX", 15)
 
 
 class Shortener(models.Model):
     url = models.CharField(max_length=220)
-    shortcode = models.CharField(max_length=15, unique=True, blank=True)
+    shortcode = models.CharField(max_length=SHORTCODE_MAX, unique=True, blank=True)
     updated = models.DateField(auto_now=True)
     tmiestamp = models.DateField(auto_now_add=True)
 
