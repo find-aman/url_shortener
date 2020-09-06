e07b0589ba0ef37993121982c0cac39801a15379
Aman Gupta <amang9578@gmail.com>
2020-09-06 23:45:01 +0530

    models modefied

--- shortener/models.py
+++ shortener/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from .utils import code_generator, create_shortcode
 
 
 class Shortener(models.Model):
     url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
+    updated = models.DateField(auto_now=True)
+    tmiestamp = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return str(self.url)
+
+    def save(self, *args, **kwargs):
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
+        super(Shortener, self).save(*args, **kwargs)
